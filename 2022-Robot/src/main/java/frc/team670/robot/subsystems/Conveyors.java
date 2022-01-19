823a1cb2b058ae114059bc07ce8fbefebd5c62f7
Mee12321 <91099630+Mee12321@users.noreply.github.com>
2022-01-18 22:03:25 -0800

    Started working on the sensor-related code

--- 2022-Robot/src/main/java/frc/team670/robot/subsystems/Conveyors.java
+++ 2022-Robot/src/main/java/frc/team670/robot/subsystems/Conveyors.java
@@ -2,8 +2,10 @@ package frc.team670.robot.subsystems;
 
 import com.revrobotics.REVLibError;
 
+import frc.team670.mustanglib.dataCollection.sensors.BeamBreak;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.utils.motorcontroller.MotorConfig.Motor_Type;
+import frc.team670.mustanglib.utils.motorcontroller.MotorConfig;
 import frc.team670.mustanglib.utils.motorcontroller.SparkMAXFactory;
 import frc.team670.mustanglib.utils.motorcontroller.SparkMAXLite;
 import frc.team670.mustanglib.utils.Logger;
@@ -20,14 +22,24 @@ import frc.team670.robot.constants.RobotMap;
 public class Conveyors extends MustangSubsystemBase 
 {
 
-    private SparkMAXLite Roller;
+    private SparkMAXLite roller;
+    private double conveyorSpeed;
 
-    private double conveyorSpeed; 
+    private boolean conveyorStates[] = {false, false, false};
 
+
+    BeamBreak beamBreak1;
+    BeamBreak beamBreak2;
+    BeamBreak beamBreak3;
     public Conveyors(int id, MotorConfig.Motor_Type type, double speed) 
     {
-        Roller = new SparkMAXLite(id, type);
+        roller = new SparkMAXLite(id, type);
         conveyorSpeed = speed;
+        //Check the dio port for the beamBreak sensors
+        beamBreak1 = new BeamBreak(0);
+        beamBreak2 = new BeamBreak(1);
+        beamBreak3 = new BeamBreak(2);
+        numBalls = 0;
     }
 
     public void run(boolean reversed) 
@@ -56,6 +68,24 @@ public class Conveyors extends MustangSubsystemBase
     public void setSpeed(double speed)
     {
         conveyorSpeed = speed;
+    }
+
+    public void updateConveyorStates () {
+        for (int i = 0; i < conveyorStates.length)
+    }
+
+    public boolean isConveyorFull() {
+        boolean conveyorCondition = false;
+        if (beamBreak3.isTriggered() == true)
+            conveyorCondition = true;
+        
+        return conveyorCondition;
+    }
+
+    public void useConveyor () {
+        if (!isConveyorFull()) {
+            run(false);
+        }
     }
 
     @Override
