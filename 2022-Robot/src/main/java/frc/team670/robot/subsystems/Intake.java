7ae84e4f21d4884066fbd931c7e1b60374219fd7
Kevin323c <91099839+Kevin323Chang@users.noreply.github.com>
2022-01-19 17:28:03 -0800

    Intake prototype

--- 2022-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
+++ 2022-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
@@ -1,11 +1,7 @@
 package frc.team670.robot.subsystems;
-
 import frc.team670.robot.constants.RobotMap;
 import frc.team670.mustanglib.utils.motorcontroller.MotorConfig.Motor_Type;
 import frc.team670.mustanglib.utils.motorcontroller.SparkMAXLite;
-
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.utils.Logger;
@@ -17,31 +13,35 @@ import frc.team670.mustanglib.utils.motorcontroller.SparkMAXFactory;
 public class Intake extends MustangSubsystemBase {
 
     private SparkMAXLite roller;
-    private Compressor compressor;
-    private Solenoid deployer;
+    private SparkMAXLite deployer;
+    
     private boolean isDeployed = false; //TODO: true for testing, change this
 
-    private double INTAKE_ROLLER_SPEED = 1.0; // From testing 2/16
+
+    private static final double INTAKE_DEPLOYER_SPEED=1.0;
+    private static final double INTAKE_ROLLER_SPEED = 1.0; 
+    
     private double INTAKE_PEAK_CURRENT = 35; // Testing
     private int exceededCurrentLimitCount = 0;
 
     public Intake() {
         // Intake roller should be inverted
         roller = SparkMAXFactory.buildFactorySparkMAX(RobotMap.INTAKE_ROLLER, Motor_Type.NEO_550);
+        deployer = SparkMAXFactory.buildFactorySparkMAX(RobotMap.INTAKE_DEPLOYER, Motor_Type.NEO_550);
         roller.setInverted(true);
         roller.setOpenLoopRampRate(1.0);
-        compressor = new Compressor(RobotMap.PCMODULE);
-        compressor.setClosedLoopControl(true);
-        deployer = new Solenoid(RobotMap.PCMODULE, RobotMap.INTAKE_DEPLOYER);
+    
+        
     }
 
-    public boolean isRolling() {
+    public boolean isRolling() { 
         return roller.get() != 0;
     }
 
     public void deploy(boolean isDeployed) {
         this.isDeployed = isDeployed;
-        deployer.set(isDeployed);
+        deployer.set(INTAKE_DEPLOYER_SPEED);
+        
     }
 
     public boolean isDeployed() {
@@ -66,7 +66,7 @@ public class Intake extends MustangSubsystemBase {
             } else {
                 exceededCurrentLimitCount = 0;
             }
-            if (exceededCurrentLimitCount >= 1){ // 4 consecutive readings higher than peak
+            if (exceededCurrentLimitCount >= 4){ // 4 consecutive readings higher than peak
                 return true;
             }
         }
@@ -76,7 +76,15 @@ public class Intake extends MustangSubsystemBase {
     }
 
     public void stop() {
-        roller.set(0);
+        roller.stopMotor();
+        retractIntake();
+        
+    }
+
+    public void retractIntake () {
+
+        deployer.set(INTAKE_DEPLOYER_SPEED * -1);
+
     }
 
     /**
@@ -88,13 +96,12 @@ public class Intake extends MustangSubsystemBase {
         if (roller == null || isSparkMaxErrored(roller)) {
             return HealthState.RED;
         }
-        if (compressor == null || deployer == null) {
+        if (deployer == null) {
             if (isDeployed) {
-                // If there's a problem with only pneumatics but the intake is already
-                // deployed, we can still roll the intake
+
                 return HealthState.YELLOW;
             }
-            // If it isn't deployed and we have a pneumatics issue, we can't do anything
+
             return HealthState.RED;
         }
         return HealthState.GREEN;
@@ -102,7 +109,7 @@ public class Intake extends MustangSubsystemBase {
 
     @Override
     public void mustangPeriodic() {
-        // SmartDashboard.putNumber("IntakeCurrent", roller.getOutputCurrent());
+        checkHealth();
     }
 
 }
