4870d88caaa40a7cc57bca269a208143e7a9bafd
smishra467 <89799108+smishra467@users.noreply.github.com>
2022-01-20 19:35:10 -0800

    adjusted climber to use 4 motors

--- 2022-Robot/src/main/java/frc/team670/robot/subsystems/Climber.java
+++ 2022-Robot/src/main/java/frc/team670/robot/subsystems/Climber.java
@@ -37,14 +37,35 @@ public class Climber extends MustangSubsystemBase {
 
     private int SMARTMOTION_SLOT = 0;
 
-    private CANPIDController controller;
-    private CANEncoder encoder;
-    private SparkMAXLite motor;
+    private CANPIDController controllerStraight;
+    private CANEncoder encoderStraight;
+    private SparkMAXLite motorStraight;
+    private CANPIDController controllerStraight2;
+    private CANEncoder encoderStraight2;
+    private SparkMAXLite motorStraight2;
+
+    private CANPIDController controllerOblique;
+    private CANEncoder encoderOblique;
+    private SparkMAXLite motorOblique;
+    private CANPIDController controllerOblique2;
+    private CANEncoder encoderOblique2;
+    private SparkMAXLite motorOblique2;
     
     private boolean onBar;
     private double target;
+    private boolean onBar2;
+    private double target2;
 
     private int currentAtHookedCount = 0;
+    private int currentAtHookedCount2 = 0;
+
+    private boolean onBarOblique;
+    private double targetOblique;
+    private boolean onBarOblique2;
+    private double targetOblique2;
+
+    private int currentAtHookedCountOblique = 0;
+    private int currentAtHookedCountOblique2 = 0;
 
     private static final float MOTOR_ROTATIONS_AT_RETRACTED = 0;
     private static final float MOTOR_ROTATIONS_AT_MAX_EXTENSION = 368;
@@ -53,19 +74,57 @@ public class Climber extends MustangSubsystemBase {
     private static final float SOFT_LIMIT_AT_EXTENSION = MOTOR_ROTATIONS_AT_MAX_EXTENSION - 10;
 
     public Climber() {
-        motor = SparkMAXFactory.buildFactorySparkMAX(RobotMap.CLIMBER_MOTOR, Motor_Type.NEO);
-        motor.setIdleMode(IdleMode.kBrake);
-        motor.setInverted(true);
-        controller = motor.getPIDController();
-        motor.enableSoftLimit(SoftLimitDirection.kForward, true);
-        motor.enableSoftLimit(SoftLimitDirection.kReverse, true);
-        motor.setSoftLimit(SoftLimitDirection.kForward, SOFT_LIMIT_AT_EXTENSION);
-        motor.setSoftLimit(SoftLimitDirection.kReverse, SOFT_LIMIT_AT_RETRACTED);
-        encoder = motor.getEncoder();
-        encoder.setPosition(MOTOR_ROTATIONS_AT_RETRACTED);
+        motorStraight = SparkMAXFactory.buildFactorySparkMAX(RobotMap.CLIMBER_MOTOR, Motor_Type.NEO);
+        motorStraight.setIdleMode(IdleMode.kBrake);
+        motorStraight.setInverted(true);
+        controllerStraight = motorStraight.getPIDController();
+        motorStraight.enableSoftLimit(SoftLimitDirection.kForward, true);
+        motorStraight.enableSoftLimit(SoftLimitDirection.kReverse, true);
+        motorStraight.setSoftLimit(SoftLimitDirection.kForward, SOFT_LIMIT_AT_EXTENSION);
+        motorStraight.setSoftLimit(SoftLimitDirection.kReverse, SOFT_LIMIT_AT_RETRACTED);
+        encoderStraight = motorStraight.getEncoder();
+        encoderStraight.setPosition(MOTOR_ROTATIONS_AT_RETRACTED);
+        setDefaultPID();
+        motorStraight2 = SparkMAXFactory.buildFactorySparkMAX(RobotMap.CLIMBER_MOTOR, Motor_Type.NEO);
+        motorStraight2.setIdleMode(IdleMode.kBrake);
+        motorStraight2.setInverted(true);
+        controllerStraight2 = motorStraight2.getPIDController();
+        motorStraight2.enableSoftLimit(SoftLimitDirection.kForward, true);
+        motorStraight2.enableSoftLimit(SoftLimitDirection.kReverse, true);
+        motorStraight2.setSoftLimit(SoftLimitDirection.kForward, SOFT_LIMIT_AT_EXTENSION);
+        motorStraight2.setSoftLimit(SoftLimitDirection.kReverse, SOFT_LIMIT_AT_RETRACTED);
+        encoderStraight2 = motorStraight2.getEncoder();
+        encoderStraight2.setPosition(MOTOR_ROTATIONS_AT_RETRACTED);
+        setDefaultPID();
+
+
+        motorOblique = SparkMAXFactory.buildFactorySparkMAX(RobotMap.CLIMBER_MOTOR, Motor_Type.NEO);
+        motorStraight.setIdleMode(IdleMode.kBrake);
+        motorOblique.setInverted(true);
+        controllerOblique = motorOblique.getPIDController();
+        motorOblique.enableSoftLimit(SoftLimitDirection.kForward, true);
+        motorOblique.enableSoftLimit(SoftLimitDirection.kReverse, true);
+        motorOblique.setSoftLimit(SoftLimitDirection.kForward, SOFT_LIMIT_AT_EXTENSION);
+        motorOblique.setSoftLimit(SoftLimitDirection.kReverse, SOFT_LIMIT_AT_RETRACTED);
+        encoderOblique = motorOblique.getEncoder();
+        encoderOblique.setPosition(MOTOR_ROTATIONS_AT_RETRACTED);
+        setDefaultPID();
+        motorOblique2 = SparkMAXFactory.buildFactorySparkMAX(RobotMap.CLIMBER_MOTOR, Motor_Type.NEO);
+        motorStraight2.setIdleMode(IdleMode.kBrake);
+        motorOblique2.setInverted(true);
+        controllerOblique2 = motorOblique2.getPIDController();
+        motorOblique2.enableSoftLimit(SoftLimitDirection.kForward, true);
+        motorOblique2.enableSoftLimit(SoftLimitDirection.kReverse, true);
+        motorOblique2.setSoftLimit(SoftLimitDirection.kForward, SOFT_LIMIT_AT_EXTENSION);
+        motorOblique2.setSoftLimit(SoftLimitDirection.kReverse, SOFT_LIMIT_AT_RETRACTED);
+        encoderOblique2 = motorOblique2.getEncoder();
+        encoderOblique2.setPosition(MOTOR_ROTATIONS_AT_RETRACTED);
         setDefaultPID();
 
         onBar = false;
+        onBar2 = false;
+        onBarOblique = false;
+        onBarOblique2 = false;
         SmartDashboard.putNumber("Climber power", 0.0);
         SmartDashboard.putBoolean("Climber deploy", false);
         SmartDashboard.putNumber("Climber motor rotations", 0);
@@ -73,29 +132,46 @@ public class Climber extends MustangSubsystemBase {
     }
 
     public void setDefaultPID() {
-        controller.setP(kP);
-        controller.setI(kI);
-        controller.setD(kD);
-        controller.setFF(kFF);
-        controller.setSmartMotionMaxVelocity(MAX_VEL, this.SMARTMOTION_SLOT);
-        controller.setSmartMotionMinOutputVelocity(MIN_VEL, this.SMARTMOTION_SLOT);
-        controller.setSmartMotionMaxAccel(MAX_ACC, this.SMARTMOTION_SLOT);
-        controller.setSmartMotionAllowedClosedLoopError(ALLOWED_ERR, this.SMARTMOTION_SLOT);
+        controllerOblique.setP(kP);
+        controllerOblique.setI(kI);
+        controllerOblique.setD(kD);
+        controllerOblique.setFF(kFF);
+        controllerOblique.setSmartMotionMaxVelocity(MAX_VEL, this.SMARTMOTION_SLOT);
+        controllerOblique.setSmartMotionMinOutputVelocity(MIN_VEL, this.SMARTMOTION_SLOT);
+        controllerOblique.setSmartMotionMaxAccel(MAX_ACC, this.SMARTMOTION_SLOT);
+        controllerOblique.setSmartMotionAllowedClosedLoopError(ALLOWED_ERR, this.SMARTMOTION_SLOT);
+
+        controllerStraight.setP(kP);
+        controllerStraight.setI(kI);
+        controllerStraight.setD(kD);
+        controllerStraight.setFF(kFF);
+        controllerStraight.setSmartMotionMaxVelocity(MAX_VEL, this.SMARTMOTION_SLOT);
+        controllerStraight.setSmartMotionMinOutputVelocity(MIN_VEL, this.SMARTMOTION_SLOT);
+        controllerStraight.setSmartMotionMaxAccel(MAX_ACC, this.SMARTMOTION_SLOT);
+        controllerStraight.setSmartMotionAllowedClosedLoopError(ALLOWED_ERR, this.SMARTMOTION_SLOT);
     }
 
     public void hookOnBar() {
         if (isHooked() && !onBar) {
-            setPower(0);
+            setPower1(0);
             onBar = true;
         }
 
+        if (isHooked2() && !onBar2) {
+            setPower2(0);
+            onBar2 = true;
+        }
+
         if (!onBar) {
-            setPower(-0.25);
+            setPower1(-0.25);
+        }
+        if (!onBar2) {
+            setPower2(-0.25);
         }
     }
 
     private boolean isHooked() {
-        double current = motor.getOutputCurrent();
+        double current = motorStraight.getOutputCurrent();
         if (current > 0.2) {
             if (current >= NORMAL_OUTPUT) {
                 currentAtHookedCount++;
@@ -110,40 +186,177 @@ public class Climber extends MustangSubsystemBase {
 
     }
 
+    private boolean isHooked2() {
+        double current = motorStraight2.getOutputCurrent();
+        if (current > 0.2) {
+            if (current >= NORMAL_OUTPUT) {
+                currentAtHookedCount2++;
+            } else {
+                currentAtHookedCount2 = 0;
+            }
+            if (currentAtHookedCount2 >= 4) { // 4 consecutive readings higher than peak
+                return true;
+            }
+        }
+        return false;
+
+    }
+
     public boolean isHookedOnBar() {
         return this.onBar;
     }
+    public boolean isHookedOnBar2() {
+        return this.onBar2;
+    }
 
-    public void setPower(double power) {
-        motor.set(power);
+    public void setPower1(double power) {
+        motorStraight.set(power);
+    }
+    public void setPower2(double power) {
+        motorStraight2.set(power);
     }
 
     public void climb(double heightCM) {
         double rotations = heightCM * ROTATIONS_PER_CM;
         target = rotations;
+        target2 = rotations;
         SmartDashboard.putNumber("Climber rotation target", rotations);
-        controller.setReference(rotations, ControlType.kSmartMotion);
+        controllerStraight.setReference(rotations, ControlType.kSmartMotion);
+        controllerStraight2.setReference(rotations, ControlType.kSmartMotion);
     }
 
     @Override
     public HealthState checkHealth() {
-        if (isSparkMaxErrored(motor)) {
+        if (isSparkMaxErrored(motorStraight) || isSparkMaxErrored(motorOblique)) {
             return HealthState.RED;
         }
         return HealthState.GREEN;
     }
 
     public boolean isAtTarget() {
-        return Math.abs(encoder.getPosition() - target) < HALF_CM;
+        return Math.abs(encoderStraight.getPosition() - target) < HALF_CM;
+    }
+
+    public boolean isAtTarget2() {
+        return Math.abs(encoderStraight2.getPosition() - target2) < HALF_CM;
     }
 
     protected double getUnadjustedMotorRotations() {
-        return this.encoder.getPosition();
+        return this.encoderStraight.getPosition();
     }
 
     protected double getMotorCurrent() {
-        return this.motor.getOutputCurrent();
+        return this.motorStraight.getOutputCurrent();
     }
+
+    protected double getUnadjustedMotorRotations2() {
+        return this.encoderStraight2.getPosition();
+    }
+
+    protected double getMotorCurrent2() {
+        return this.motorStraight2.getOutputCurrent();
+    }
+
+    public void hookOnBarOblique() {
+        if (isHookedOblique() && !onBarOblique) {
+            setPowerOblique1(0);
+            onBarOblique = true;
+        }
+
+        if (isHookedOblique2() && !onBarOblique2) {
+            setPowerOblique2(0);
+            onBarOblique2 = true;
+        }
+
+        if (!onBarOblique) {
+            setPowerOblique1(-0.25);
+        }
+        if (!onBarOblique2) {
+            setPowerOblique2(-0.25);
+        }
+    }
+
+    private boolean isHookedOblique() {
+        double current = motorOblique.getOutputCurrent();
+        if (current > 0.2) {
+            if (current >= NORMAL_OUTPUT) {
+                currentAtHookedCountOblique++;
+            } else {
+                currentAtHookedCountOblique = 0;
+            }
+            if (currentAtHookedCountOblique >= 4) { // 4 consecutive readings higher than peak
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    private boolean isHookedOblique2() {
+        double current = motorOblique2.getOutputCurrent();
+        if (current > 0.2) {
+            if (current >= NORMAL_OUTPUT) {
+                currentAtHookedCountOblique2++;
+            } else {
+                currentAtHookedCountOblique2 = 0;
+            }
+            if (currentAtHookedCountOblique2 >= 4) { // 4 consecutive readings higher than peak
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public boolean isHookedOnBarOblique() {
+        return this.onBarOblique;
+    }
+
+    public boolean isHookedOnBarOblique2() {
+        return this.onBarOblique2;
+    }
+
+    public void setPowerOblique1(double power) {
+        motorOblique.set(power);
+    }
+
+    public void setPowerOblique2(double power) {
+        motorOblique2.set(power);
+    }
+
+    public void climbOblique(double heightCM) {
+        double rotations = heightCM * ROTATIONS_PER_CM;
+        targetOblique = rotations;
+        targetOblique2 = rotations;
+        SmartDashboard.putNumber("Climber rotation target", rotations);
+        controllerOblique.setReference(rotations, ControlType.kSmartMotion);
+        controllerOblique2.setReference(rotations, ControlType.kSmartMotion);
+    }
+
+    public boolean isAtTargetOblique() {
+        return Math.abs(encoderOblique.getPosition() - targetOblique) < HALF_CM;
+    }
+
+    protected double getUnadjustedMotorRotationsOblique() {
+        return this.encoderOblique.getPosition();
+    }
+
+    protected double getMotorCurrentOblique() {
+        return this.motorOblique.getOutputCurrent();
+    }
+
+    public boolean isAtTargetOblique2() {
+        return Math.abs(encoderOblique2.getPosition() - targetOblique2) < HALF_CM;
+    }
+
+    protected double getUnadjustedMotorRotationsOblique2() {
+        return this.encoderOblique2.getPosition();
+    }
+
+    protected double getMotorCurrentOblique2() {
+        return this.motorOblique2.getOutputCurrent();
+    }
+
 
     @Override
     public void mustangPeriodic() {
@@ -152,7 +365,7 @@ public class Climber extends MustangSubsystemBase {
     }
 
     public void test() {
-        setPower(SmartDashboard.getNumber("Climber power", 0.0));
+        setPower1(SmartDashboard.getNumber("Climber power", 0.0));
         SmartDashboard.putNumber("Climber motor rotations", getUnadjustedMotorRotations());
     }
 
