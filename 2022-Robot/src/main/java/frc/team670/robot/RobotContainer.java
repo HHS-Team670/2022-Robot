1214220f271e8ac30f5bb37542657d6049fb58f3
clearlyconfused <56371094+tarinimaram@users.noreply.github.com>
2022-01-21 17:13:15 -0800

    delete old command

--- 2022-Robot/src/main/java/frc/team670/robot/RobotContainer.java
+++ 2022-Robot/src/main/java/frc/team670/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import frc.team670.mustanglib.dataCollection.sensors.BeamBreak;
 import frc.team670.mustanglib.commands.MustangScheduler;
 import frc.team670.mustanglib.utils.Logger;
 import frc.team670.mustanglib.utils.MustangController;
+import frc.team670.robot.commands.auton.ATarmacEdge4BallTest;
 import frc.team670.robot.commands.auton.ATarmacFlushed1Ball;
 import frc.team670.robot.commands.auton.LeftTarmac2Shoot;
 import frc.team670.robot.commands.auton.MoveForwards;
@@ -55,11 +56,12 @@ public class RobotContainer extends RobotContainerBase {
    * @return the command to run in autonomous
    */
   public MustangCommand getAutonomousCommand() {
-    MustangCommand autonCommand = new ATarmacFlushed1Ball(driveBase);
+    MustangCommand autonCommand = new ATarmacEdge4BallTest(driveBase);
 
     Logger.consoleLog("autonCommand: %s", autonCommand);
     return autonCommand;
   }
+
 
   public void autonomousInit() {
     Logger.consoleLog("autoInit called");
