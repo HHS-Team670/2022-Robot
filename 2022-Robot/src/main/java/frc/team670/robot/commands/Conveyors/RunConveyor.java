292b9e9007df59e87ce58c3981211b4046971674
arghunter <acgo8888@gmail.com>
2022-01-22 22:29:11 -0800

    fixed some conveyors commands

--- 2022-Robot/src/main/java/frc/team670/robot/commands/Conveyors/RunConveyor.java
+++ 2022-Robot/src/main/java/frc/team670/robot/commands/Conveyors/RunConveyor.java
@@ -1,17 +1,17 @@
-package frc.team670.robot.commands.Conveyors;
-
+package frc.team670.robot.commands.conveyors;
 import java.util.HashMap;
 import java.util.Map;
 
 import frc.team670.mustanglib.utils.Logger;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.team670.mustanglib.commands.MustangCommand;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
 import frc.team670.robot.subsystems.Conveyors;
 
-public class RunConveyor extends CommandBase implements MustangCommand {
+public class RunConveyor extends InstantCommand implements MustangCommand {
 
     private Conveyors conveyors;
     private Map<MustangSubsystemBase, HealthState> healthReqs;
@@ -19,7 +19,7 @@ public class RunConveyor extends CommandBase implements MustangCommand {
 
     public RunConveyor(Conveyors conveyors, boolean intaking)
     {
-        this.conveyors=conveyors;
+        this.conveyors = conveyors;
         addRequirements(conveyors);
         healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
         healthReqs.put(conveyors, HealthState.YELLOW);
@@ -30,13 +30,11 @@ public class RunConveyor extends CommandBase implements MustangCommand {
 
     public void initialize() {
         Logger.consoleLog("Running Conveyors");
+        conveyors.runConveyors(intaking);
         
     }
     
-    public void execute() {
-        conveyors.runConveyors(intaking);
 
-    }
 
     @Override
     public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
