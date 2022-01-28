20eb8da9dbcdf85d3e098334fe939e8489d7df1e
Dhruv B <91165182+dbhargava199@users.noreply.github.com>
2022-01-27 20:39:36 -0800

    Updated ATarmacEdge4Ball Command

--- 2022-Robot/src/main/java/frc/team670/robot/commands/auton/ATarmacEdge4Ball.java
+++ 2022-Robot/src/main/java/frc/team670/robot/commands/auton/ATarmacEdge4Ball.java
@@ -12,13 +12,26 @@ import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
 import frc.team670.robot.subsystems.DriveBase;
 
 //change to ATarmacEdge4Ball for 2022 drivebase
-public class ATarmacEdge4BallTest extends SequentialCommandGroup implements MustangCommand {
+public class ATarmacEdge4Ball extends SequentialCommandGroup implements MustangCommand {
     private Map<MustangSubsystemBase, HealthState> healthReqs;
-    private Trajectory trajectory;
+    private Trajectory trajectory, trajectory2, trajectory3, trajectory4, trajectory5, trajectory6;
 
-    public ATarmacEdge4BallTest(DriveBase driveBase) {
+    public ATarmacEdge4Ball(DriveBase driveBase) {
         //change to ATarmacEdge4Ball for 2022 drivebase
-        trajectory = PathPlanner.loadPath("test", 2.0, 1);
+        
+        trajectory = PathPlanner.loadPath("ATarmacEdge4BallP1", 2.0, 1);
+        //pickup ball
+        trajectory2 = PathPlanner.loadPath("ATarmacEdge4BallP2", 2.0, 1);
+        //shooting balls
+        trajectory3 = PathPlanner.loadPath("ATarmacEdge4BallP3", 2.0, 1);
+        //pickup ball
+        trajectory4 = PathPlanner.loadPath("ATarmacEdge4BallP4", 2.0, 1);
+        //pickup ball
+        trajectory5 = PathPlanner.loadPath("ATarmacEdge4BallP5", 2.0, 1);
+        //pickup ball
+        trajectory6 = PathPlanner.loadPath("ATarmacEdge4BallP6", 2.0, 1);
+        //shoot ball
+        
         //Logger.consoleLog("Loaded path " + trajectory.toString());
         healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
         healthReqs.put(driveBase, HealthState.GREEN);
