82941ce6a942774263b48eea867c75de0ea560a2
sonicunleshed21 <rishboss0@gmail.com>
2022-01-27 19:05:48 -0800

    Added ATarmacEdge2Ball path

---
+++ 2022-Robot/src/main/java/frc/team670/robot/commands/auton/ATarmacEdge2Ball.java
@@ -0,0 +1,44 @@
+package frc.team670.robot.commands.auton;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.team670.mustanglib.commands.MustangCommand;
+import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
+import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
+import frc.team670.robot.subsystems.DriveBase;
+
+public class ATarmacEdge2Ball extends SequentialCommandGroup implements MustangCommand {
+    private Map<MustangSubsystemBase, HealthState> healthReqs;
+    private Trajectory trajectory;
+
+    public ATarmacEdge2Ball(DriveBase driveBase) {
+        trajectory = PathPlanner.loadPath("ATarmacEdge2Ball", 1.0, 0.5);
+        //Logger.consoleLog("Loaded path " + trajectory.toString());
+        healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
+        healthReqs.put(driveBase, HealthState.GREEN);
+
+        //Logger.consoleLog("Initial Pose " + trajectory.getStates().get(0).poseMeters);
+
+        driveBase.resetOdometry(trajectory.getStates().get(0).poseMeters);
+        addCommands(
+            getTrajectoryFollowerCommand(trajectory, driveBase)
+        );
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+
+    @Override
+    public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
+        // TODO Auto-generated method stub
+        return healthReqs;
+    }
+
+}
