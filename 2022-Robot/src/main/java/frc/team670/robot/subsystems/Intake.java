a60934aab93b1da6ddbe41ba50909211d15de8b7
smishra467 <89799108+smishra467@users.noreply.github.com>
2022-01-29 12:21:10 -0800

    added code to stop deployer when it gets deployed or retracts back

--- 2022-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
+++ 2022-Robot/src/main/java/frc/team670/robot/subsystems/Intake.java
@@ -3,6 +3,7 @@ package frc.team670.robot.subsystems;
 import frc.team670.robot.constants.RobotMap;
 import frc.team670.mustanglib.utils.motorcontroller.MotorConfig.Motor_Type;
 import frc.team670.mustanglib.utils.motorcontroller.SparkMAXLite;
+import com.revrobotics.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.utils.Logger;
@@ -13,17 +14,41 @@ import frc.team670.mustanglib.utils.motorcontroller.SparkMAXFactory;
 */
 public class Intake extends MustangSubsystemBase {
 
-  private SparkMAXLite roller;
-  private SparkMAXLite deployer;
-  
-  private boolean isDeployed = false; //TODO: true for testing, change this
-
 
   private static final double INTAKE_DEPLOYER_SPEED=1.0;
   private static final double INTAKE_ROLLER_SPEED = 1.0; 
+  private static final double DEPLOYER_TICKS_NOT_DEPLOYED = 0; // TODO: change this later when we get the motor and subsystem
+  private static final double DEPLOYER_TICKS_DEPLOYED = 0;
+
+  private double kP = 0;
+  private double kI = 0;
+  private double kD = 0;
+  private double kFF = 0;
+
+  private double POW = 0.25;
+
+  // SmartMotion constants
+  private static final double MAX_ACC = 0;
+  private static final double MIN_VEL = 0;
+  private static final double MAX_VEL = 0;
+
+  private static final double ALLOWED_ERR = 0;
+
+  private static final double NORMAL_OUTPUT = 0; // Todo: this should be the current output when running normally
+  private static final double ROTATIONS_PER_CM = 0; // gearing is 50:1
+  private static final double HALF_CM = 0.5 * ROTATIONS_PER_CM;
+
+  private int SMARTMOTION_SLOT = 0;
   
   private double INTAKE_PEAK_CURRENT = 35; // Testing
   private int exceededCurrentLimitCount = 0;
+  private SparkMAXLite roller;
+  private SparkMAXLite deployer;
+
+  private CANEncoder deployerEncoder;
+  private CANPIDController deployerController;
+  
+  private boolean isDeployed = false; //TODO: true for testing, change this
 
   public Intake() {
     // Intake roller should be inverted
@@ -31,8 +56,18 @@ public class Intake extends MustangSubsystemBase {
     deployer = SparkMAXFactory.buildFactorySparkMAX(RobotMap.INTAKE_DEPLOYER, Motor_Type.NEO_550);
     roller.setInverted(true);
     roller.setOpenLoopRampRate(1.0);
-  
-    
+    deployerEncoder = deployer.getEncoder();
+    deployerEncoder.setPosition(DEPLOYER_TICKS_NOT_DEPLOYED);
+    deployerController = deployer.getPIDController();
+
+    deployerController.setP(kP);
+    deployerController.setI(kI);
+    deployerController.setD(kD);
+    deployerController.setFF(kFF);
+    deployerController.setSmartMotionMaxVelocity(MAX_VEL, this.SMARTMOTION_SLOT);
+    deployerController.setSmartMotionMinOutputVelocity(MIN_VEL, this.SMARTMOTION_SLOT);
+    deployerController.setSmartMotionMaxAccel(MAX_ACC, this.SMARTMOTION_SLOT);
+    deployerController.setSmartMotionAllowedClosedLoopError(ALLOWED_ERR, this.SMARTMOTION_SLOT);
   }
 
   public boolean isRolling() { 
@@ -42,7 +77,7 @@ public class Intake extends MustangSubsystemBase {
   public void deploy(boolean isDeployed) {
     this.isDeployed = isDeployed;
     deployer.set(INTAKE_DEPLOYER_SPEED);
-    
+    deployerController.setReference(DEPLOYER_TICKS_DEPLOYED, ControlType.kSmartMotion);
   }
 
   public boolean isDeployed() {
@@ -83,6 +118,7 @@ public class Intake extends MustangSubsystemBase {
 
   public void retractIntake () {
     deployer.set(INTAKE_DEPLOYER_SPEED * -1);
+    deployerController.setReference(DEPLOYER_TICKS_NOT_DEPLOYED, ControlType.kSmartMotion);
   }
 
   /**
