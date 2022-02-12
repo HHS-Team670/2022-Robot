5f471b9e30bbd69759e1facb63ac135f2bb998fd
clearlyconfused <56371094+tarinimaram@users.noreply.github.com>
2022-02-11 17:28:09 -0800

    Update LeftTarmac2Shoot.java

--- 2022-Robot/src/main/java/frc/team670/robot/commands/auton/LeftTarmac2Shoot.java
+++ 2022-Robot/src/main/java/frc/team670/robot/commands/auton/LeftTarmac2Shoot.java
@@ -5,13 +5,10 @@ import java.util.Map;
 import com.pathplanner.lib.PathPlanner;
 
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.team670.mustanglib.commands.MustangCommand;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
-import frc.team670.mustanglib.utils.Logger;
 import frc.team670.robot.subsystems.DriveBase;
 
 
@@ -19,11 +16,10 @@ import frc.team670.robot.subsystems.DriveBase;
 public class LeftTarmac2Shoot extends SequentialCommandGroup implements MustangCommand {
 
     private Map<MustangSubsystemBase, HealthState> healthReqs;
-    private Trajectory trajectory, trajectory2;
+    private Trajectory trajectory;
 
     public LeftTarmac2Shoot(DriveBase driveBase) {
-        trajectory = PathPlanner.loadPath("LeftTarmac2ShootPt1", 1.0, 0.5);
-        trajectory2 = PathPlanner.loadPath("LeftTarmac2ShootPt2", 1.0, 0.5);
+        trajectory = PathPlanner.loadPath("LeftTarmac2Shoot", 1.0, 0.5);
         //Logger.consoleLog("Loaded path " + trajectory.toString());
         healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
         healthReqs.put(driveBase, HealthState.GREEN);
@@ -35,7 +31,7 @@ public class LeftTarmac2Shoot extends SequentialCommandGroup implements MustangC
             //drive forward
             //intake
             //drive back
-            getTrajectoryFollowerCommand(trajectory2, driveBase), 
+            getTrajectoryFollowerCommand(trajectory, driveBase), 
             new StopDriveBase(driveBase)
             //shoot 2 balls
         );
