b777e1309b9f845be15d4a4d2cc9e1b24f2cb0fc
Mihir-A <68079673+Mihir-A@users.noreply.github.com>
2022-02-13 14:08:32 -0800

    Update FieldConstants.java

--- 2022-Robot/src/main/java/frc/team670/robot/constants/FieldConstants.java
+++ 2022-Robot/src/main/java/frc/team670/robot/constants/FieldConstants.java
@@ -4,38 +4,55 @@ package frc.team670.robot.constants;
  * All values in meters unless otherwise specified
  */
 public class FieldConstants {
-    public static double DISTANCE_TO_GOAL_FROM_START = 3.24;//1.0; // TODO: Change this later when we find out
+    public static final double DISTANCE_TO_GOAL_FROM_START = 3.24;//1.0; // TODO: Change this later when we find out
 
     // from game manual:
-    public static double HEIGHT_LOW_GOAL = 1.04;
-    public static double HEIGHT_HIGH_GOAL = 2.64;
-    public static double HEIGHT_HIGH_PLATE = 1.71;
-    public static double WIDTH_BASE = 2.72;
-    public static double SMALL_WHATEVER_LEFT_OF_LOGO_ON_PG_26 = 0.34;
-    public static double WIDTH_LOGO = 1.17;
-    public static double WIDTH_LOW_GOAL = 1.53;
-    public static double LOW_TO_FENDER = 0.03;
-    public static double WIDTH_HIGH_GOAL = 1.22;
-    public static double FENDER_TO_LOW_GOAL_OPENING = 0.1;
-    public static double FENDER_TO_CENTER_OF_LOW_GOAL = 0.86;
-    public static double TARMAC_TO_CENTER_OF_GOAL = 2.15;
-    public static double 
+    public static final double HEIGHT_LOW_GOAL = 1.04;
+    public static final double HEIGHT_HIGH_GOAL = 2.64;
+    public static final double HEIGHT_HIGH_PLATE = 1.71;
+    public static final double WIDTH_BASE = 2.72;
+    public static final double SMALL_WHATEVER_LEFT_OF_LOGO_ON_PG_26 = 0.34;
+    public static final double WIDTH_LOGO = 1.17;
+    public static final double WIDTH_LOW_GOAL = 1.53;
+    public static final double LOW_TO_FENDER = 0.03;
+    public static final double WIDTH_HIGH_GOAL = 1.22;
+    public static final double FENDER_TO_LOW_GOAL_OPENING = 0.1;
+    public static final double FENDER_TO_CENTER_OF_LOW_GOAL = 0.86;
+
+    //Hub
+    public static final double DIAMETER_OF_UPPER_HUB = 1.22;
+    public static final double DIAMETER_OF_LOWER_HUB = 1.53;
+    public static final double ORIGIN_TO_CENTER_OF_GOAL = 9.20;
+
+    
+    //TARMAC
+    public static final double TARMAC_TO_EDGE_OF_HUB = 2.15;
+    public static final double TARMAC_TO_CENTER_OF_GOAL = 3.51;
+    
+    
+    //Hangar
+    public static final double HANGAR_DISTANCE_Y_FROM_ORIGIN = 5.28;
+    public static final double HANGAR_DISTANCE_X_FROM_ORIGIN = 3.27;
+    public static final double HANGAR_OPPONENTS_DISTANCE_Y_FROM_ORIGIN = 2.95;
+    public static final double HANGAR_OPPONENTS_DISTANCE_X_FROM_ORIGIN = 13.19;
+
+
 
     // Position of the hub
 //     e HUB is 8 ft. 11 in.
 // (~272 cm) wide by 8 ft. 11 in. (~272 cm) deep by 8 ft. 8 in. (~264 cm) tall
 
-    public static double HUB_WIDTH = 2.72;
-    public static double HUB_HEIGHT = 2.64;
+    public static final double HUB_WIDTH = 2.72;
+    public static final double HUB_HEIGHT = 2.64;
 
     // field dimensions
     // Each FIELD for RAPID REACT is a 27 ft. (~823 cm) by 54 ft. (~1646 cm) carpeted area
-    public static double FIELD_HEIGHT = 8.23;
-    public static double FIELD_WIDTH = 16.46;
+    public static final double FIELD_HEIGHT = 8.23;
+    public static final double FIELD_WIDTH = 16.46;
 
     // hub pose
-    public static double HUB_POSE_Y = FIELD_HEIGHT / 2;
-    public static double HUB_POSE_X = FIELD_WIDTH / 2;
+    public static final double HUB_POSE_Y = FIELD_HEIGHT / 2;
+    public static final double HUB_POSE_X = FIELD_WIDTH / 2;
 
     
 }
