570f0d30e59e8a85b18591262736fe2a7c531631
Katia Bravo <KatiaBravo@users.noreply.github.com>
2022-02-19 13:55:20 -0800

    add some constants

--- 2022-Robot/src/main/java/frc/team670/robot/constants/FieldConstants.java
+++ 2022-Robot/src/main/java/frc/team670/robot/constants/FieldConstants.java
@@ -22,13 +22,14 @@ public class FieldConstants {
 
     public static final double LOW_HUB_DIAMETER = 1.53;
     public static final double HIGH_HUB_DIAMETER = 1.22;    
+    public static final double HUB_RADIUS = HIGH_HUB_DIAMETER / 2;
     public static final double LOW_TO_FENDER = 0.03;
     public static final double HUB_BASE_WIDTH = 2.72;
     public static final double FENDER_TO_LOW_HUB_OPENING = 0.1;
     public static final double FENDER_TO_CENTER_OF_LOW_HUB = 0.86;
     public static final double HUB_POSE_Y = FIELD_HEIGHT / 2;
     public static final double HUB_POSE_X = FIELD_WIDTH / 2;
-
+    
     //Tarmac
     public static final double TARMAC_TO_EDGE_OF_HUB = 2.15;
     public static final double TARMAC_TO_CENTER_OF_GOAL = 3.51;
@@ -38,5 +39,4 @@ public class FieldConstants {
     public static final double HANGAR_DISTANCE_X_FROM_ORIGIN = 3.27;
     public static final double HANGAR_OPPONENTS_DISTANCE_Y_FROM_ORIGIN = 2.95;
     public static final double HANGAR_OPPONENTS_DISTANCE_X_FROM_ORIGIN = 13.19;
-
 }
