02210030495066dc8332a3fb9cf46d36ce3e4f5a
RiyaGupta1234 <riyagsms@gmail.com>
2022-02-18 20:03:11 -0800

    clean up vision

--- 2022-Robot/src/main/java/frc/team670/robot/subsystems/Vision.java
+++ 2022-Robot/src/main/java/frc/team670/robot/subsystems/Vision.java
@@ -7,14 +7,10 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.subsystems.VisionSubsystemBase;
-import frc.team670.mustanglib.utils.Logger;
 import frc.team670.robot.constants.FieldConstants;
 import frc.team670.robot.constants.RobotConstants;
 import frc.team670.robot.constants.RobotMap;
@@ -24,19 +20,19 @@ import frc.team670.robot.constants.RobotMap;
  * Stores values off of NetworkTables for easy retrieval and gives them
  * Listeners to update the stored values as they are changed.
  * 
- * @author ctychen, lakshbhambhani
+ * @author ethan chang, katia bravo, riya gupta
  */
-public class Vision extends VisionSubsystemBase {
+public class Vision extends VisionSubsystemBase{
 
     private Solenoid cameraLEDs = new Solenoid(RobotMap.PCMODULE, PneumaticsModuleType.CTREPCM, RobotMap.VISION_LED_PCM);
     private PhotonCamera camera = new PhotonCamera(RobotConstants.VISION_CAMERA);
     private Pose2d startPose = new Pose2d(0, 0, new Rotation2d(0));
 
-    private double distance;
     private double distanceNoError;
-    private double angle;
-    private double visionCapTime;
-    private boolean hasTarget;
+    protected double distance;
+    protected double angle;
+    protected double visionCapTime;
+    protected boolean hasTarget;
 
     /*
     * 11' 8" --> trying to do 35 feet --tested with 30 feet
@@ -51,75 +47,9 @@ public class Vision extends VisionSubsystemBase {
     * Speckle Rejection: 100; Target Grouping: 2ormore
     */ 
 
-    //should not have a constructor
+    
 
-    // public Vision(){
-    //     // pose = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
-    //     // transformedPose = pose;
-    //     // targetPose = new Pose2d(FieldConstants.DISTANCE_TO_GOAL_FROM_START, 0.0, new Rotation2d(0.0));
-    // }
-
-    // public Vision(String cameraType) {
-    //     camera = new PhotonCamera(cameraType);
-    //     // pose = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
-    //     transformedPose = pose;
-    //     targetPose = new Pose2d(FieldConstants.DISTANCE_TO_GOAL_FROM_START, 0.0, new Rotation2d(0.0));
-    // }
-
-    public void setCameraName (String cameraName) {
-        camera = new PhotonCamera(cameraName);
-    }
-
-    public boolean hasTarget(){
-        return hasTarget;
-    }
-
-    /**
-     * 
-     * @return distance, in inches, from the camera to the target
-     */
-    private void processImage() {
-        try{
-            var result = camera.getLatestResult();
-
-            if(result.hasTargets()){
-                hasTarget = true;
-                angle = camera.getLatestResult().getTargets().get(0).getYaw();
-                distance = PhotonUtils.calculateDistanceToTargetMeters(
-                        RobotConstants.CAMERA_HEIGHT_METERS,
-                        FieldConstants.HIGH_HUB_HEIGHT,
-                        Units.degreesToRadians(RobotConstants.CAMERA_ANGLE_DEGREES),
-                        Units.degreesToRadians(result.getBestTarget().getPitch()));
-
-                distanceNoError = distance + calculateError(angle);
-                
-                visionCapTime = Timer.getFPGATimestamp() - result.getLatencyMillis()/1000;
-            } else {
-                hasTarget = false;
-                // Logger.consoleLog("NO TARGET DETECTED");
-            }
-            
-        } catch(Exception e){
-            // MustangNotifications.reportWarning(e.toString());
-            Logger.consoleLog("NT for vision not found %s", e.getStackTrace());
-        }
-    }
-
-    public double getDistanceToTargetM() {
-        return hasTarget ? distance : RobotConstants.VISION_ERROR_CODE;
-    }
-
-    public double getDistanceToTargetCm() {
-        return getDistanceToTargetM() * 100;
-    }
-
-    public double getDistanceToTargetInches() {
-        return getDistanceToTargetM() * 100 / 2.54;
-    }
-
-    public double getAngleToTarget() {
-        return hasTarget ? angle : RobotConstants.VISION_ERROR_CODE;
-    }
+   
 
     public VisionMeasurement getVisionMeasurements(double heading, Pose2d targetPose, Pose2d cameraOffset) {
         if (hasTarget){
@@ -132,11 +62,7 @@ public class Vision extends VisionSubsystemBase {
         return null;
     }
 
-    public void setStartPose(double x, double y, double angle) {
-        //TODO: check that coordinate is correct
-        startPose = new Pose2d(x, y, new Rotation2d(angle));
-    }
-
+   
     public Transform2d changeInPose(double heading) {
         // d_0 is the distance from the start position of the robot to the high hub
         // d_c is the distance from the start position of the robot to the current position (calculated by the Law of Cosines)
@@ -150,50 +76,14 @@ public class Vision extends VisionSubsystemBase {
         return new Transform2d(new Translation2d(x , y), new Rotation2d(angleStartToCurr));
     }
 
-    public double getVisionCaptureTime() {
-        return visionCapTime;
-    }
 
-    public void turnOnLEDs() {
-        cameraLEDs.set(true);
-    }
-
-    public void turnOffLEDs() {
-        cameraLEDs.set(false);
-    }
-
-    public void LEDSwitch(boolean on) {
-        cameraLEDs.set(on);
-    }
-
-    public void testLEDS() {
-        cameraLEDs.set(SmartDashboard.getBoolean("LEDs on", true));
-    }
-
-    @Override
-    public HealthState checkHealth() {
-        //TODO: change to be more useful for debugging vision
-        return HealthState.GREEN;
-    }
-
-    @Override
     public void mustangPeriodic() {
-        processImage();
+        super.processImage(RobotConstants.CAMERA_HEIGHT_METERS, FieldConstants.HIGH_HUB_HEIGHT, RobotConstants.CAMERA_ANGLE_DEGREES);
         
         if (hasTarget) {
             SmartDashboard.putNumber("Vision Distance", distance);
             SmartDashboard.putNumber("Vision Distance Without Error", distanceNoError);
             SmartDashboard.putNumber("Vision Angle (yaw)", angle);
-        }
-    }
-
-    public class VisionMeasurement{
-        public Pose2d pose;
-        public double capTime;
-
-        public VisionMeasurement(Pose2d pose, double capTime){
-            this.capTime = capTime;
-            this.pose = pose;
         }
     }
 
@@ -212,8 +102,5 @@ public class Vision extends VisionSubsystemBase {
         return a * Math.pow(yaw, 4) + b * Math.pow(yaw, 3) + c * Math.pow(yaw, 2) + d * yaw + f;
 
     }
-
-    @Override
-    public void debugSubsystem() {}
 
 }
