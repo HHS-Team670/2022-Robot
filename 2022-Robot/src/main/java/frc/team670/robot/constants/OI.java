6d6c42c44a04b4236405812ef4f9a0bbe6b03092
ethan <ethanychang17@gmail.com>
2022-02-25 16:24:03 -0800

    update OI for climbing

--- 2022-Robot/src/main/java/frc/team670/robot/constants/OI.java
+++ 2022-Robot/src/main/java/frc/team670/robot/constants/OI.java
@@ -2,18 +2,20 @@ package frc.team670.robot.constants;
 
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.team670.mustanglib.commands.drive.teleop.XboxRocketLeague.FlipDriveDirection;
-import frc.team670.mustanglib.commands.vision.SetVisionLEDs;
 import frc.team670.mustanglib.commands.vision.ToggleLEDs;
 import frc.team670.mustanglib.constants.OIBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.utils.MustangController;
 import frc.team670.mustanglib.utils.MustangController.XboxButtons;
+import frc.team670.robot.commands.climber.ExtendClimber;
+import frc.team670.robot.commands.climber.FullClimb;
 import frc.team670.robot.commands.deployer.ToggleIntake;
 import frc.team670.robot.commands.routines.StopAll;
 import frc.team670.robot.commands.routines.intake.EmptyRobot;
 import frc.team670.robot.commands.routines.intake.RunIntakeWithConveyor;
 import frc.team670.robot.commands.routines.shoot.ShootAllBalls;
 import frc.team670.robot.commands.shooter.StopShooter;
+import frc.team670.robot.subsystems.ClimberSystem;
 import frc.team670.robot.subsystems.ConveyorSystem;
 import frc.team670.robot.subsystems.Deployer;
 import frc.team670.robot.subsystems.DriveBase;
@@ -26,16 +28,20 @@ public class OI extends OIBase {
   private static MustangController driverController = new MustangController(RobotMap.DRIVER_CONTROLLER_PORT);
   private static MustangController operatorController = new MustangController(RobotMap.OPERATOR_CONTROLLER_PORT);
 
+  // operator controls
   private static JoystickButton triggerIntaking = new JoystickButton(getOperatorController(), XboxButtons.X);
   private static JoystickButton triggerOuttaking = new JoystickButton(getOperatorController(), XboxButtons.B);
   private static JoystickButton stopAll = new JoystickButton(getOperatorController(), XboxButtons.A);
   private static JoystickButton toggleIntake = new JoystickButton(getOperatorController(), XboxButtons.Y);
   private static JoystickButton stopShooter = new JoystickButton(getOperatorController(), XboxButtons.RIGHT_BUMPER);
   private static JoystickButton shootAllBalls = new JoystickButton(getOperatorController(), XboxButtons.LEFT_BUMPER);
-  
+  private static JoystickButton fullClimb = new JoystickButton(getOperatorController(), XboxButtons.LEFT_JOYSTICK_BUTTON);
+  private static JoystickButton testExtendClimber = new JoystickButton(getOperatorController(), XboxButtons.RIGHT_JOYSTICK_BUTTON);
+
+  // driver controls
   private static JoystickButton toggleReverseDrive = new JoystickButton(getDriverController(), XboxButtons.LEFT_BUMPER);
   private static JoystickButton turnVisionLEDsOn = new JoystickButton(getDriverController(), XboxButtons.X);
-  private static JoystickButton turnVisionLEDsOff = new JoystickButton(getDriverController(), XboxButtons.B);
+  private static JoystickButton turnVisionLEDsOff = new JoystickButton(getDriverController(), XboxButtons.B); //TODO: make this be used
   //private static JoystickButton resetNavx = new JoystickButton(getDriverController(), XboxButtons.LEFT_BUMPER);
 
   private DriveBase driveBase;
@@ -72,6 +78,11 @@ public class OI extends OIBase {
     Intake intake = (Intake) subsystemBases[3];
     Deployer deployer = (Deployer) subsystemBases [4];
     Vision vision = (Vision) subsystemBases [5];
+    ClimberSystem climberSystem = (ClimberSystem) subsystemBases[6];
+
+    fullClimb.whenPressed(new FullClimb(climberSystem));
+
+    testExtendClimber.whenPressed(new ExtendClimber(climberSystem.getVerticalClimber()));
 
     toggleReverseDrive.whenPressed(new FlipDriveDirection());
 
