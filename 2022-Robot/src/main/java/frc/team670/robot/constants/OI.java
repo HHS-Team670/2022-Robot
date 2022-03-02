72fb24c868295686f76fadbdf86e22ae435c7d1d
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2022-03-01 21:47:48 -0800

    enable intake controls

--- 2022-Robot/src/main/java/frc/team670/robot/constants/OI.java
+++ 2022-Robot/src/main/java/frc/team670/robot/constants/OI.java
@@ -1,6 +1,7 @@
 package frc.team670.robot.constants;
 
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.team670.mustanglib.commands.drive.teleop.ResetNavX;
 import frc.team670.mustanglib.commands.drive.teleop.XboxRocketLeague.FlipDriveDirection;
 import frc.team670.mustanglib.commands.vision.SetVisionLEDs;
 import frc.team670.mustanglib.commands.vision.ToggleLEDs;
@@ -8,7 +9,7 @@ import frc.team670.mustanglib.constants.OIBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.utils.MustangController;
 import frc.team670.mustanglib.utils.MustangController.XboxButtons;
-// import frc.team670.robot.commands.deployer.ToggleIntake;
+import frc.team670.robot.commands.deployer.ToggleIntake;
 import frc.team670.robot.commands.routines.StopAll;
 import frc.team670.robot.commands.routines.intake.EmptyRobot;
 import frc.team670.robot.commands.routines.intake.RunIntakeWithConveyor;
@@ -26,17 +27,17 @@ public class OI extends OIBase {
   private static MustangController driverController = new MustangController(RobotMap.DRIVER_CONTROLLER_PORT);
   private static MustangController operatorController = new MustangController(RobotMap.OPERATOR_CONTROLLER_PORT);
 
-  // private static JoystickButton triggerIntaking = new JoystickButton(getOperatorController(), XboxButtons.X);
+  private static JoystickButton triggerIntaking = new JoystickButton(getOperatorController(), XboxButtons.X);
   private static JoystickButton triggerOuttaking = new JoystickButton(getOperatorController(), XboxButtons.B);
   private static JoystickButton stopAll = new JoystickButton(getOperatorController(), XboxButtons.A);
-  // private static JoystickButton toggleIntake = new JoystickButton(getOperatorController(), XboxButtons.Y);
+  private static JoystickButton toggleIntake = new JoystickButton(getOperatorController(), XboxButtons.Y);
   private static JoystickButton stopShooter = new JoystickButton(getOperatorController(), XboxButtons.RIGHT_BUMPER);
   private static JoystickButton shootAllBalls = new JoystickButton(getOperatorController(), XboxButtons.LEFT_BUMPER);
   
   private static JoystickButton toggleReverseDrive = new JoystickButton(getDriverController(), XboxButtons.LEFT_BUMPER);
   private static JoystickButton turnVisionLEDsOn = new JoystickButton(getDriverController(), XboxButtons.X);
   private static JoystickButton turnVisionLEDsOff = new JoystickButton(getDriverController(), XboxButtons.B);
-  //private static JoystickButton resetNavx = new JoystickButton(getDriverController(), XboxButtons.LEFT_BUMPER);
+  private static JoystickButton resetNavx = new JoystickButton(getDriverController(), XboxButtons.LEFT_BUMPER);
 
   private DriveBase driveBase;
 
@@ -75,7 +76,7 @@ public class OI extends OIBase {
 
     toggleReverseDrive.whenPressed(new FlipDriveDirection());
 
-    // triggerIntaking.whenPressed(new RunIntakeWithConveyor(intake, conveyorSystem));
+    triggerIntaking.whenPressed(new RunIntakeWithConveyor(intake, conveyorSystem));
     triggerOuttaking.whenPressed(new EmptyRobot(intake, conveyorSystem, deployer));
 
     stopAll.whenPressed((new StopAll(intake, conveyorSystem, shooter)));
@@ -83,11 +84,11 @@ public class OI extends OIBase {
     shootAllBalls.whenPressed(new ShootAllBalls(driveBase, conveyorSystem, shooter, vision));
     stopShooter.whenPressed((new StopShooter(shooter)));
 
-    // toggleIntake.whenPressed(new ToggleIntake(deployer));
+    toggleIntake.whenPressed(new ToggleIntake(deployer));
 
     turnVisionLEDsOn.whenPressed(new SetVisionLEDs(true, vision));
     turnVisionLEDsOff.whenPressed(new SetVisionLEDs(false, vision));
 
-    //resetNavx.whenPressed(new ResetNavX(driveBase.getNavX()));
+    resetNavx.whenPressed(new ResetNavX(driveBase.getNavX()));
   }
 }
