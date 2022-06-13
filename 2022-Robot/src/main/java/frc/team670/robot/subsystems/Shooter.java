8ded7107c358c77af0440eda3787d6cdf561063e
Justin Hwang <justin@goliath.org>
2022-06-12 19:36:15 -0700

    Added commented-out arrays back in because they're used for regression.

--- 2022-Robot/src/main/java/frc/team670/robot/subsystems/Shooter.java
+++ 2022-Robot/src/main/java/frc/team670/robot/subsystems/Shooter.java
@@ -339,3 +339,93 @@ public class Shooter extends MustangSubsystemBase {
         SmartDashboard.putNumber("Ultrasonic Distance", getUltrasonicDistanceInMeters());
     }
 }
+
+/**
+private static final double[] MEASURED_DISTANCE_LOW_METER = {
+    0.18415,
+    0.48895,
+    0.79375,
+    1.09855,
+    1.40335,
+    2.01295,
+    2.62255,
+    3.23215,
+    3.84175
+};
+
+private static final double[] MEASURED_LOW_RPM = {
+    1550,
+    1600,
+    1800,
+    2000,
+    2250,
+    2600,
+    2800,
+    3000,
+    3500
+};
+
+private static final double[] MEASURED_DISTANCE_HIGH_METER = {
+    1.60655,
+    1.91135,
+    2.21615,
+    2.97,
+    3.13055,
+    3.66,
+    4.54,
+    4.65455
+};
+
+private static final double[] MEASURED_HIGH_RPM = {
+    3150,
+    3350,
+    3650,
+    3700,
+    3850,
+    3900,
+    4200,
+    4500
+};
+
+private static final double[] MEASURED_DISTANCE_HIGH_METER_RISKY = {
+    1.60655,
+    1.91135,
+    2.21615,
+    2.5,
+    2.97,
+    3.13055,
+    3.66,
+    3.99,
+    4.54,
+    4.65455, 
+    4.9,
+    5.2,
+    5.5,
+    5.8,
+    6.1,
+    6.4
+};
+
+private static final double[] MEASURED_HIGH_RPM_RISKY = {
+    3150,
+    3350,
+    3650,
+    3650,
+    3700,
+    3850,
+    3900,
+    4200,
+    4200,
+    4500,
+    4700,
+    4975,
+    5200,
+    5250,
+    5315,
+    5250,
+};
+
+private static final LinearRegression speedAtDistanceForHighGoal = new LinearRegression(
+        MEASURED_DISTANCE_HIGH_METER,
+        MEASURED_HIGH_RPM);
+ */
