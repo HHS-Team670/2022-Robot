20cede026549a85728c3fecc123f0d047a9d4cef
Justin Hwang <justin@goliath.org>
2022-06-13 22:25:52 -0700

    Removed debug Logger & SmartDashboard prints

--- 2022-Robot/src/main/java/frc/team670/robot/commands/auton/StopDriveBase.java
+++ 2022-Robot/src/main/java/frc/team670/robot/commands/auton/StopDriveBase.java
@@ -4,12 +4,10 @@ package frc.team670.robot.commands.auton;
 import java.util.HashMap;
 import java.util.Map;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team670.mustanglib.commands.MustangCommand;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
-import frc.team670.mustanglib.utils.Logger;
 import frc.team670.robot.subsystems.DriveBase;
 
 /**
@@ -24,14 +22,9 @@ public class StopDriveBase extends CommandBase implements MustangCommand{
         this.driveBase = driveBase;
         healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
         healthReqs.put(driveBase, HealthState.GREEN);
-
-        Logger.consoleLog("CALLED STOP DRIVE BASE");
-        Logger.consoleLog("CALLED STOP DRIVE BASE");
     }
 
     public void initialize() {
-        SmartDashboard.putString("Stop drive base called", "yea");
-
         super.initialize();
         driveBase.stop();
     }
