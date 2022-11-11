9843d59e6fb2bc0908968e6260877a9c7fb0bd57
Dhruv Jain <djain859@student.fuhsd.org>
2022-11-10 19:48:26 -0800

    Conveyor System Commit (Dhruv Jain)
    Made code to intake and count up to two balls, and to stop the motors as needed.
    The motors are set to speed 10 by default.

--- 2022-Robot/src/main/java/frc/team670/robot/subsystems/ConveyorSystem.java
+++ 2022-Robot/src/main/java/frc/team670/robot/subsystems/ConveyorSystem.java
@@ -16,6 +16,69 @@ import frc.team670.robot.constants.RobotMap;
  * 
  * @author Armaan, Soham, Edward
  */
+
+
+
 public class ConveyorSystem extends MustangSubsystemBase {
+    SparkMAXLite conveyorMotor1;
+    SparkMAXLite conveyorMotor2;
+    BeamBreak beam1;
+    BeamBreak beam2;
+    int ballCount = 0;
+    
+    //Declare vairables, conveyors and beambreaks.
+    public ConveyorSystem(){
+        conveyorMotor1 = SparkMAXFactory.buildSparkMAX(RobotMap.INTAKE_CONVEYOR_MOTOR, SparkMAXFactory.defaultConfig, Motor_Type.NEO_550);
+        conveyorMotor2 = SparkMAXFactory.buildSparkMAX(RobotMap.SHOOTER_CONVEYOR_MOTOR, SparkMAXFactory.defaultConfig, Motor_Type.NEO_550);
+        beam1= new BeamBreak(RobotMap.INTAKE_CONVEYOR_BEAMBREAK);
+        beam2= new BeamBreak(RobotMap.SHOOTER_CONVEYOR_BEAMBREAK);
+        
+
+        
+    }
+
+    
+        //Set speed of motor 1
+    public void setC1(double speed){
+        conveyorMotor1.set(speed);
+    }
+        
+    //Set speed of motor 2
+    public void setC2(double speed){
+        conveyorMotor2.set(speed);
+    }
+
+
+    @Override
+    public HealthState checkHealth() {
+        return null;
+    }
+
+
+    @Override
+    public void mustangPeriodic() {
+        if (beam2.isTriggered() == true && ballCount == 0){
+            ballCount++;
+            setC2(0);
+        } 
+        if (beam1.isTriggered() == true && ballCount == 1){
+            setC1(0);
+            ballCount++;
+        } 
+        if (ballCount == 2){
+            setC1(0);
+            setC2(0);
+        }
+    }
+
+
+    @Override
+    public void debugSubsystem() {
+        
+    }
+
+
+    
 
 }
+
