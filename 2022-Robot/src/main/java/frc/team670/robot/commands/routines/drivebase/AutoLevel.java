a194c16f5cc4b8e9a0f7d88f3fc04afd905c05aa
JyVenom <72235651+JyVenom@users.noreply.github.com>
2023-01-11 22:23:14 -0800

    Update AutoLevel.java

--- 2022-Robot/src/main/java/frc/team670/robot/commands/routines/drivebase/AutoLevel.java
+++ 2022-Robot/src/main/java/frc/team670/robot/commands/routines/drivebase/AutoLevel.java
@@ -1,54 +1,60 @@
 package frc.team670.robot.commands.routines.drivebase;
 
+import java.util.HashMap;
 import java.util.Map;
-
-import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team670.mustanglib.commands.MustangCommand;
-import frc.team670.mustanglib.dataCollection.sensors.NavX;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
 import frc.team670.robot.subsystems.DriveBase;
-import frc.team670.mustanglib.utils.Logger;
-import frc.team670.robot.constants.RobotMap;
 
 public class AutoLevel extends CommandBase implements MustangCommand {
-    DriveBase driveBase;
-    double target = 0;
+    private DriveBase driveBase;
+    private Map<MustangSubsystemBase, HealthState> healthReqs;
+
+    private double target = 0;
 
     public AutoLevel(DriveBase driveBase) {
         this.driveBase = driveBase;
+        
+        this.healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
+        this.healthReqs.put(driveBase, HealthState.GREEN);
+        //what is addRequirements? do i need it?
     }
 
     @Override
     public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
-        // TODO Auto-generated method stub
-        return null;
+        return healthReqs;
     }
 
-    @Override
-    public void initialize() {
-        // TODO Auto-generated method stub
-        // gyro.calibrate();
-    }
+    // @Override
+    // public void initialize() {
+    //     // probably don't need
+    //     // gyro.calibrate();
+    // }
 
     @Override
     public void execute() {
         double angle = driveBase.getPitch();
-        SmartDashboard.putString("Pitch: ", ""+angle);
-        
-        double kp = 0.05;
+        SmartDashboard.putString("Pitch: ", "" + angle);
 
+        double kp = 0.05;
+        //for fun i want to try doing verlett integration on the gyro and fusion (maybe just a filter) that with the accelrometer to get my own pitch measurement and compare with the getPitch method
         double adjustment = MathUtil.clamp((target - angle) * kp, -1, 1); // check if i called clamp correctly 🤣
-        driveBase.curvatureDrive(adjustment, 0, false); // negative since undo/counteract
-        //Seems that negative is wrong dir? 
+        driveBase.curvatureDrive(adjustment, 0, false); // negative adjustment since undo/counteract (update, negative gives oposite direction?)
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return super.isFinished(); //return false;
+        //maybe do time based (like stop after 5 secs) or angle change based (it has settled down and robot ramp system is basically static equalibrium)
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        driveBase.stop(); //idk if we want this here but for now imma go with yes
     }
 }
